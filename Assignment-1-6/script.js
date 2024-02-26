bde5c047dc6e6503b1672494b62f83a9c34689c4
bimandas1 <biman.das@innoraft.com>
2024-02-26 19:45:27 +0530

    Files deleted

--- Assignment-1-6/script.js
+++
@@ -1,7 +0,0 @@
-var firstName = document.querySelector("#first-name");
-var lastName = document.querySelector("#last-name");
-var fullName = document.querySelector("#full-name");
-
-function updateFullName() {
-    fullName.value = `${firstName.value} ${lastName.value}`;
-}
