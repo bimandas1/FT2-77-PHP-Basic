2027b143894896a28a282b04e6df7c0efda0bddf
bimandas1 <biman.das@innoraft.com>
2024-04-01 11:39:47 +0530

    FT2-80: Added exception handle, solved some mistakes.

--- Assignment-2/index.php
+++ Assignment-2/index.php
@@ -2,7 +2,7 @@
 
 require __DIR__ . '/Mail.php';
 
-if(isset($_POST['email'])) {
+if (isset($_POST['email'])) {
   $mail = new Mail();
   $mail->sendMail($_POST['email']);
 }
@@ -10,16 +10,19 @@ if(isset($_POST['email'])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP Adv assignment-2</title>
 </head>
+
 <body>
-  <form action="/" method="POST">
+  <form action="<?= $_SERVER["PHP_SELF"] ?>" method="POST">
     <label for="email"> Email : </label>
     <input type="email" name="email">
     <input type="submit">
   </form>
 </body>
+
 </html>
